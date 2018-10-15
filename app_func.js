1b64330c53e40308289fa9c4add37df52bc80152
Jonas <lind.jonas94@gmail.com>
2018-10-15 11:24:12 +0200

    added functionallity for graph drawing, cytoscape.min.js is needed se cytoscape js

--- app_func.js
+++ app_func.js
@@ -1,20 +1,77 @@
+window.onload = function()
+{
+  document.getElementById("main_content").innerHTML =
+    document.getElementById("hidden_home").innerHTML;
+}
+
+var resultEnabled = false;
+
+function home()
+{
+  document.getElementById("main_content").innerHTML =
+  document.getElementById("hidden_home").innerHTML;
+
+  if(resultEnabled)
+  {
+    document.getElementById("main_content_container").style.height = '300%';
+    document.getElementById("result_area").style.height = '33.33%';
+    document.getElementById("input_area").style.height = '33.33%';
+    document.getElementById("introduction_area").style.height = '33.33%';
+  }
+  else
+  {
+    document.getElementById("main_content_container").style.height = '200%';
+    document.getElementById("result_area").style.height = '0%';
+    document.getElementById("input_area").style.height = '50%';
+    document.getElementById("introduction_area").style.height = '50%';
+  }
+}
+
+function help()
+{
+  if(resultEnabled)
+  {
+    document.getElementById("hidden_home").innerHTML =
+      document.getElementById("main_content").innerHTML;
+  }
+  document.getElementById("main_content").innerHTML =
+    document.getElementById("hidden_help").innerHTML;
+}
+
+function about()
+{
+  if(resultEnabled)
+  {
+    document.getElementById("hidden_home").innerHTML =
+      document.getElementById("main_content").innerHTML;
+  }
+  document.getElementById("main_content").innerHTML =
+    document.getElementById("hidden_about").innerHTML;
+}
+
+function hide_intro_button()
+{
+  var element = document.getElementById("intro_button");
+  if(element != undefined) {
+    element.parentNode.removeChild(element);
+  }
+}
+
 function go_to_input()
 {
-  // scroll down
-  window.scrollBy(0, 1500);
+  window.scroll({top:1100,left:0,behavior:'smooth'});
+  hide_intro_button();
 }
 
 function enable_result()
 {
-
-  // enable result part.
+  resultEnabled = true;
+  hide_intro_button();
   document.getElementById("main_content_container").style.height = '300%';
   document.getElementById("result_area").style.height = '33.3%';
   document.getElementById("input_area").style.height = '33.3%';
   document.getElementById("introduction_area").style.height = '33.3%';
 
-  //
   document.getElementById("result_area").innerHTML = hidden_result.innerHTML;
-  window.scrollBy(0, 1000);
-  // scroll down.
+  window.scroll({top:2000,left:0,behavior:'smooth'});
 }
--- index.js
+++ index.js
@@ -1,53 +1,113 @@
 function findCyclesMain() {
 
 
-    enable_result();
-    document.getElementById("enumerated_cycles").value = "Calculating...";
-    var find_just_one_cycle = !document.getElementById("enumerate_all").checked;
-    var schema = document.getElementById("schema_text").value;
+  enable_result();
+  document.getElementById("enumerated_cycles").value = "Calculating...";
+  var find_just_one_cycle = !document.getElementById("enumerate_all").checked;
+  var schema = document.getElementById("schema_text").value + "\n";
 
-    const jsObj = convert(schema);
-    const graph = convertToGraph(jsObj);
+  const jsObj = convert(schema);
+  const graph = convertToGraph(jsObj);
 
-    const res = detectCycles(graph, find_just_one_cycle);
+  var _vertices = [];
+  var _edges = [];
+  cy = []; // empty if its not the first time
 
-    if(res.foundCycle) {
-	document.getElementById("label_output").textContent = ("Found cycles");
+  var incrementedge = 0;
+  for(var vertex in graph.graph)
+  {
+    // added for graph drawing purposes.
+    //{ data: { id: 'a' } },
+    //{ data: { id: 'a"e', weight: 1, source: 'a', target: 'e' } },
+    //console.log(graph.graph[vertex].vertexID);
+    _vertices.push({data:{id: graph.graph[vertex].vertexID}});
+    for(var edge in graph.graph[vertex].referenceList)
+    {
+
+        var label = graph.graph[vertex].referenceList[edge].label;
+        var referenceID = graph.graph[vertex].referenceList[edge].reference.vertexID;
+
+      _edges.push({data:{id:(incrementedge++)+label, weight:1 ,source:vertex, target:referenceID}});
     }
-    else {
-	document.getElementById("label_output").textContent = ("Found no cycles");
-    }
+  }
 
-    document.getElementById("enumerated_cycles").value = "";
+  const res = detectCycles(graph.graph, find_just_one_cycle);
 
-/*    for( var sc in res.cycles) {
+  if(res.foundCycle) {
+    document.getElementById("label_output").textContent = ("Found cycles");
+  }
+  else {
+    document.getElementById("label_output").textContent = ("Found no cycles");
+  }
+
+  document.getElementById("enumerated_cycles").value = "";
+
+
+  // create graph here!
+  for( var sc in res.cycles) {
     var string = "{ ";
-	for ( var vert in res.cycles[sc] ) {
-	    string += res.cycles[sc][vert]["vertex"].vertexID;
-	    if( res.cycles[sc][vert]["vertex"].vertexType === "interface") string += "(i)";
-	    string += "[" + res.cycles[sc][vert]["refLabel"] + "]";
-	    string += ", ";
-	}
-	string = string.slice(0,-4);
-	string += " }";
-	document.getElementById("enumerated_cycles").value += string + "\n";
-	}*/
-
-    for( var sc in res.cycles) {
-	var string = "{ ";
-	for ( var vert in res.cycles[sc] ) {
-	    string += res.cycles[sc][vert]["vertex"].vertexID;
-	    if( res.cycles[sc][vert]["refLabel"] === "#interface_ref") {
-		string += " <~implements~ ";
-	    }
-	    else if (res.cycles[sc][vert]["refLabel"] === "#union_ref") {
-		string += " -union-> ";
-	    }
-	    else string += " -[" + res.cycles[sc][vert]["refLabel"] + "]-> ";
-	    //string += ", ";
-	}
-	string = string.slice(0,-7);
-	string += " }";
-	document.getElementById("enumerated_cycles").value += string + "\n";
+    for ( var vert in res.cycles[sc] ) {
+      string += res.cycles[sc][vert]["vertex"].vertexID;
+      if( res.cycles[sc][vert]["refLabel"] === "#interface_ref") {
+        string += " <~implements~ ";
+      }
+      else if (res.cycles[sc][vert]["refLabel"] === "#union_ref") {
+        string += " -union-> ";
+      }
+      else string += " -[" + res.cycles[sc][vert]["refLabel"] + "]-> ";
+      //string += ", ";
     }
+    string = string.slice(0,-7);
+    string += " }";
+    document.getElementById("enumerated_cycles").value += string + "\n";
+  }
+
+  // cytoscape from here and on.
+
+  var cy = cytoscape({
+    container: document.getElementById('cy'),
+
+    boxSelectionEnabled: true,
+    autounselectify: true,
+
+
+    style: cytoscape.stylesheet()
+    .selector('node')
+    .css({
+      'content': 'data(id)'
+    })
+    .selector('edge')
+    .css({
+      'curve-style': 'bezier',
+      'target-arrow-shape': 'triangle',
+      'width': 5,
+      'line-color': '#ddd',
+      'target-arrow-color': '#555'
+    })
+    .selector('.highlighted')
+    .css({
+      'background-color': '#61bffc',
+      'line-color': '#61bffc',
+      'target-arrow-color': '#61bffc',
+      'transition-property': 'background-color, line-color, target-arrow-color',
+      'transition-duration': '0.5s'
+    }),
+
+    elements: {
+      nodes: _vertices,
+        //{ data: { id: 'a' } },
+      edges: _edges
+        //{ data: { id: 'a"e', weight: 1, source: 'a', target: 'e' } },
+    },
+    styleEnabled : true,
+
+    gravity: 1,
+
+    layout: {
+      name: 'cose',
+      directed: true,
+      roots: '#a',
+      padding: 10
+    }
+  });
 }
